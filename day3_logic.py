25583c43243e3f57b2959487b7abc6f38130ee4b
MukeshlabX <mukeshvasanthan7@gmail.com>
2025-07-13 18:23:33 +0530

    Create day3_logic.py

---
+++ day3_logic.py
@@ -0,0 +1,10 @@
+age= int(input("enter your age"))
+if age<18:
+  print("your are minor")
+elif age>18
+  print("you are major")
+else:
+  print("enter age")
+
+for i in range(1,6):
+  print("count",i)
